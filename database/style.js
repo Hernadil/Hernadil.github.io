91124cbb77628ba4cfeef1fb6fbbbf4b6c0908b0
Hernadil <159261708+Hernadil@users.noreply.github.com>
2024-07-20 15:13:29 +0200

    Delete database directory

--- database/style.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const inputs = document.querySelectorAll('input[type="text"], input[type="password"], input[type="email"]');
-
-    inputs.forEach(input => {
-        input.addEventListener('input', () => {
-            input.style.backgroundColor = 'transparent';
-        });
-    });
-});
